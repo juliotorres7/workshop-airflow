c267fb466d3e7a9e2c7e05b9cc23570d72b21881
Julio Torres <julio.torres7@outlook.com>
2025-12-13 12:04:05 -0300

    adicionando a etl braba

--- dags/main.py
+++ dags/main.py
@@ -1,15 +1,16 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.decorators import task
 from datetime import datetime
 
-def helloworld():
-    print("Hellow World")
+with DAG(
+    dag_id="hello_world_dag",
+    start_date=datetime(2024, 12, 13),
+    schedule="* * * * *",
+    catchup=False,
+) as dag:
 
-with DAG(dag_id="hello_world_dag",
-         start_date=datetime(2925,12,13),
-         schedule_interval="* * * * *",
-         catchup=False) as dag:
-    
-    task1 = (function) task_id: Literal['hello_world']
-            task_id="hello_world",
-            python_callable=helloworld
+    @task
+    def helloworld():
+        print("Hello World")
+
+    helloworld()
